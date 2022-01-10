89c349b57d214453a05a540d60a13db0dfd165b3
jiyuujin <jiyuujinunite@gmail.com>
2022-01-10 09:13:53 +0900

    LINE authentication setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,46 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
-import { authState } from '../plugins/firebase'
-import AuthContext from '../lib/AuthContext'
-import authReducer from '../lib/AuthReducer'
 
 import '../static/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
-  const [state, dispatch] = useReducer(authReducer.reducer, authReducer.initialState)
+  const [liffObject, setLiffObject] = useState<any>(null)
+
+  const [profileName, setProfileName] = useState<string>('')
 
   useEffect(() => {
-    return authState(dispatch)
+    import('@line/liff').then((liff: any) => {
+      liff
+        .init({ liffId: process.env.NEXT_APP_LIFF_ID })
+        .then(() => {
+          if (!liff.isLoggedIn()) {
+            liff.login({})
+          }
+          setLiffObject(liff)
+          liff
+            .getProfile()
+            .then((profile: any) => {
+              setProfileName(profile.displayName)
+            })
+            .catch((err: any) => {
+              console.error({ err })
+            })
+        })
+        .catch((err) => {
+          console.error({ err })
+        })
+    })
   }, [])
+
+  pageProps.liff = liffObject
+  pageProps.profileName = profileName
 
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
-      <AuthContext.Provider value={state}>
-        <Component {...pageProps} />
-      </AuthContext.Provider>
+      <Component {...pageProps} />
     </>
   )
 }
