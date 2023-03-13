6053fa993c02f5e0876e035f80ac2c9f3bb2ec0f
yuma-kitamura <jiyuujinunite@gmail.com>
2023-03-13 11:55:54 +0900

    refactor: add hooks

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import { signOut } from '../plugins/firebase'
 
-type Props = {
-  //
+interface SignOutButtonProps {
+  logout: () => void
 }
 
-export const SignOutButton = (props: Props) => {
-  const handleClick = () => {
-    signOut()
-  }
-
+export const SignOutButton = ({ logout }: SignOutButtonProps) => {
   return (
     <button
-      onClick={handleClick}
+      onClick={logout}
       type="button"
       className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,13 @@
-import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
+import { useLine } from '../hooks/useLine'
 
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
-  const [liffObject, setLiffObject] = useState<any>(null)
+  const { liffObject, status } = useLine()
 
-  const [profileName, setProfileName] = useState<string>('')
-
-  const [pictureUrl, setPictureUrl] = useState<string>('')
-
-  useEffect(() => {
-    import('@line/liff').then((liff: any) => {
-      liff
-        .init({ liffId: process.env.NEXT_APP_LIFF_ID })
-        .then(() => {
-          if (!liff.isLoggedIn()) {
-            liff.login({})
-          }
-          setLiffObject(liff)
-          liff
-            .getProfile()
-            .then((profile: any) => {
-              setProfileName(profile.displayName)
-              setPictureUrl(profile.pictureUrl)
-            })
-            .catch((err: any) => {
-              console.error({ err })
-            })
-        })
-        .catch((err) => {
-          console.error({ err })
-        })
-    })
-  }, [])
-
-  pageProps.liff = liffObject
-  pageProps.profileName = profileName
-  pageProps.pictureUrl = pictureUrl
+  pageProps.liffObject = liffObject
+  pageProps.status = status
 
   return (
     <>
